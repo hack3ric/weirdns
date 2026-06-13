5f00953733b148e6c506c50a715dadbef068fedb
Eric Long <i@hack3r.moe>
2026-06-13 23:09:12 +0800

    try to fix notify

--- src/notify.rs
+++ src/notify.rs
@@ -8,18 +8,18 @@ use std::os::linux::net::SocketAddrExt;
 use std::os::unix::net::SocketAddr;
 
 pub fn ready() {
-  notify("READY=1");
+  if let Err(error) = notify("READY=1") {
+    eprintln!("systemd notify failed: {error}");
+  }
 }
 
 #[cfg(unix)]
-fn notify(message: &str) {
+fn notify(message: &str) -> std::io::Result<()> {
   let Some(socket_path) = std::env::var_os("NOTIFY_SOCKET") else {
-    return;
+    return Ok(());
   };
 
-  let Ok(socket) = UnixDatagram::unbound() else {
-    return;
-  };
+  let socket = UnixDatagram::unbound()?;
 
   #[cfg(target_os = "linux")]
   {
@@ -27,21 +27,39 @@ fn notify(message: &str) {
     use std::path::Path;
 
     let path = socket_path.as_os_str().as_bytes();
-    let result = if let Some(name) = path.strip_prefix(b"@") {
-      SocketAddr::from_abstract_name(name).and_then(|addr| socket.send_to_addr(message.as_bytes(), &addr).map(|_| ()))
-    } else {
-      socket.send_to(message.as_bytes(), Path::new(&socket_path)).map(|_| ())
-    };
+    if path.is_empty() || (path[0] != b'/' && path[0] != b'@') {
+      return Err(std::io::Error::new(
+        std::io::ErrorKind::InvalidInput,
+        "NOTIFY_SOCKET must start with '/' or '@'",
+      ));
+    }
 
-    let _ = result;
+    if let Some(name) = path.strip_prefix(b"@") {
+      let addr = SocketAddr::from_abstract_name(name)?;
+      socket.connect_addr(&addr)?;
+    } else {
+      socket.connect(Path::new(&socket_path))?;
+    }
   }
 
   #[cfg(not(target_os = "linux"))]
   {
     use std::path::Path;
-    let _ = socket.send_to(message.as_bytes(), Path::new(&socket_path));
+    socket.connect(Path::new(&socket_path))?;
+  }
+
+  let written = socket.send(message.as_bytes())?;
+  if written == message.len() {
+    Ok(())
+  } else {
+    Err(std::io::Error::new(
+      std::io::ErrorKind::WriteZero,
+      "short write to NOTIFY_SOCKET",
+    ))
   }
 }
 
 #[cfg(not(unix))]
-fn notify(_: &str) {}
+fn notify(_: &str) -> std::io::Result<()> {
+  Ok(())
+}
